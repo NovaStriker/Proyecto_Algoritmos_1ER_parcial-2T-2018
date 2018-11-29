7e9c4241abbbfcc3974383f3a10b927e67207a1e
NovaStriker <johnaldoc@hotmail.com>
2018-11-28 23:25:30 -0500

    main terminado
    ya esta

--- pry_algoritmos_1er_parcial.py
+++ pry_algoritmos_1er_parcial.py
@@ -1,5 +1,7 @@
 from random import randint
 import time
+import os.path
+from pathlib import Path
 
 salir = False
 
@@ -14,20 +16,105 @@ while (salir==False):
 
     if (opcion == "1"):
         print ("REALIZAR ANÁLISIS")
-        print ("hola : " + opcion)
+        nombreArchivo = input("Escriba el nombre de su archivo ubicado en la carpeta: ")
+        nombreArchivo = nombreArchivo + ".txt"
+
+        if (os.path.isfile(".\\" + nombreArchivo)):
+            print("El archivo " + nombreArchivo + " ha sido encontrado")
+            print("\n")
+
+            archivoAnalisis = open(".\\" + nombreArchivo,"r")
+            salirDeAnalisis = False
+
+            salirEscogerCantidad = False
+
+            while (salirEscogerCantidad == False):
+                cantidadNumeros = input("Ingrese la cantidad de números que desea tomar del archivo (Max 1000): ")
+                if(str.isdigit(cantidadNumeros)):
+                    cantidadNumeros = int(cantidadNumeros)
+                else:
+                    print("No ha ingresado un número válido")
+                contador = 0
+                if (cantidadNumeros <= 1000):
+                    arregloAnalisis = [0]*cantidadNumeros
+                    for j in archivoAnalisis:
+                        #print(archivoAnalisis.readline())
+                        arregloAnalisis[contador] = int(j)
+                        contador = contador + 1
+                        if(contador == cantidadNumeros):
+                            break
+
+                    archivoAnalisis.close()
+                    salirEscogerCantidad = True
+                    #print(arregloAnalisis)
+
+            while (salirDeAnalisis == False):
+
+                print("Escoja el tipo de analisis que desea")
+                print("\n")
+
+                print("(1) Comparación Insertion Sort vs Merge Sort")
+                print("(2) Comparación Insertion Sort vs Quick Sort")
+                print("(3) Comparación Merge Sort vs Quick Sort")
+                print("(4) Comparar todos los algoritmos")
+                print("(5) Volver al menú principal")
+
+                opcionComparacion = input("Escriba una opcion: ")
+                print("\n")
+            
+                if (opcionComparacion == "1"):
+                    print("Insertion Sort vs Merge Sort")
+                    arregloA = arregloAnalisis
+                    arregloB = arregloAnalisis
+                    #---------------- Insertar Arreglos ----------------
+                    #---------------- Insertar Arreglos ----------------
+                elif (opcionComparacion == "2"):
+                    print("Insertion Sort vs Quick Sort")
+                    arregloA = arregloAnalisis
+                    arregloB = arregloAnalisis
+                    #---------------- Insertar Arreglos ----------------
+                    #---------------- Insertar Arreglos ----------------
+                elif (opcionComparacion == "3"):
+                    print("Merge Sort vs Quick Sort")
+                    arregloA = arregloAnalisis
+                    arregloB = arregloAnalisis
+                    #---------------- Insertar Arreglos ----------------
+                    #---------------- Insertar Arreglos ----------------
+                elif (opcionComparacion == "4"):
+                    print("Comparar todos los algoritmos")
+                    arregloA = arregloAnalisis
+                    arregloB = arregloAnalisis
+                    arregloC = arregloAnalisis
+                    #---------------- Insertar Arreglos ----------------
+                    #---------------- Insertar Arreglos ----------------
+                elif (opcionComparacion == "5"):
+                    print("Volviendo al menú principal...")
+                    salirDeAnalisis = True
+                else:
+                    print("La opción " + opcionComparacion + " no es válida")
+
+        else:
+            print("El archivo " + nombreArchivo + " no existe \n")
+            print("Intente generar un archivo aleatorio.txt desde la opción del menú principal")
+            print("\n")
 
     elif (opcion == "2"):
         print ("GENERAR ARCHIVO ALEATORIO")
+        archivoAleatorio = open(".\\aleatorio.txt","w")
+        elementos = 1000
 
-        listaAleatoria = [0]*1000
-        for i in range(1000):
+        listaAleatoria = [0]*elementos
+        for i in range(elementos):
             listaAleatoria[i] = randint(1000000, 9999999)
+            archivoAleatorio.write(str(listaAleatoria[i]) + "\n")
 
-        print(listaAleatoria)
+        archivoAleatorio.close()
+        #print(listaAleatoria)
+        print ("Archivo generado!\n")
 
     elif (opcion == "3"):
-        print ("SALIR")
-        print ("Gracias por usar el sistema de comparación")
+        print ("SALIENDO...")
+        print ("Gracias por usar el sistema de comparación \n")
         salir = True
 
     else:
